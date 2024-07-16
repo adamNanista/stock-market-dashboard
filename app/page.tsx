f078898527854cbd969525a141b163cbace71fa8
Adam Naništa <nanista@totalmoney.sk>
2024-07-16 10:43:17 +0200

    working blog

--- app/page.tsx
+++ app/page.tsx
@@ -2,41 +2,33 @@
 
 import useSWR from "swr";
 
-const fetcher = (...args: Parameters<typeof fetch>) =>
-  fetch(...args).then((res) => res.json());
-
-interface Ticker {
-  ticker: string;
-}
+const fetcher = (...args: Parameters<typeof fetch>) => fetch(...args).then((res) => res.json());
 
 function TickerPicker() {
-  const { data, isLoading } = useSWR(
-    "https://api.polygon.io/v3/reference/tickers?active=true&limit=10&apiKey=rLdG0SNWBFY4CfpFmtA1l3uMpo7k5eXR",
-    fetcher
-  );
+	const { data, isLoading } = useSWR("https://api.polygon.io/v3/reference/tickers?active=true&limit=10&apiKey=rLdG0SNWBFY4CfpFmtA1l3uMpo7k5eXR", fetcher);
 
-  if (isLoading) return <div>Loading...</div>;
-  if (!data) return <div>Fail</div>;
+	if (isLoading) return <div>Loading...</div>;
+	if (!data) return <div>Fail</div>;
 
-  return (
-    <ul className="flex flex-wrap space-x-4">
-      {data.results.map((ticker: Ticker, index: number) => {
-        return (
-          <li key={index}>
-            <a href="#" data-ticker={ticker.ticker}>
-              {ticker.ticker}
-            </a>
-          </li>
-        );
-      })}
-    </ul>
-  );
+	return (
+		<ul className="flex flex-wrap space-x-4">
+			{data.results.map((ticker: { ticker: string }, index: number) => {
+				return (
+					<li key={index}>
+						<a href={`/stock/${ticker.ticker}`} data-ticker={ticker.ticker}>
+							{ticker.ticker}
+						</a>
+					</li>
+				);
+			})}
+		</ul>
+	);
 }
 
 export default function Home() {
-  return (
-    <main>
-      <TickerPicker />
-    </main>
-  );
+	return (
+		<main>
+			<TickerPicker />
+		</main>
+	);
 }
