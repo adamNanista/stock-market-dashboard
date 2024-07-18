95041bf05ef67d51385aaecf3ecb28abdf292deb
Adam Naništa <nanista@totalmoney.sk>
2024-07-18 17:10:24 +0200

    data fetch in chart component

--- app/components/priceChart.tsx
+++ app/components/priceChart.tsx
@@ -5,6 +5,22 @@ import { Chart } from "react-chartjs-2";
 
 ChartJS.register(...registerables);
 
-export default function PriceChart({ params }: { params: { labels: string[]; values: number[] } }) {
-	return <Chart type="line" data={{ labels: params.labels, datasets: [{ label: "Price", data: params.values, tension: 0.5, pointHoverBorderWidth: 8, pointBorderWidth: 4, borderWidth: 4 }] }} options={{ plugins: { legend: { display: false } } }} />;
+async function getData({ params }: { params: { slug: string } }) {
+	const data = await fetch(`https://api.polygon.io/v2/aggs/ticker/${params.slug}/range/1/day/2024-07-10/2024-07-17?adjusted=true&sort=asc&apiKey=rLdG0SNWBFY4CfpFmtA1l3uMpo7k5eXR`).then((res) => res.json());
+
+	return data;
+}
+
+export default async function PriceChart(params: { slug: string }) {
+	const data = await getData({ params });
+
+	const labels = data.results?.map((result: { t: number }) => new Date(result.t).toLocaleDateString());
+	const values = data.results?.map((result: { c: number }) => result.c);
+
+	const chartData = {
+		labels: labels,
+		values: values,
+	};
+
+	return <>{data.results && <Chart type="line" data={{ labels: chartData.labels, datasets: [{ label: "Price", data: chartData.values, tension: 0.5, pointHoverBorderWidth: 8, pointBorderWidth: 4, borderWidth: 4 }] }} options={{ plugins: { legend: { display: false } } }} />}</>;
 }
--- app/stock/[slug]/page.tsx
+++ app/stock/[slug]/page.tsx
@@ -8,37 +8,22 @@ export async function generateStaticParams() {
 	}));
 }
 
-async function getDetailData({ params }: { params: { slug: string } }) {
+async function getData({ params }: { params: { slug: string } }) {
 	const data = await fetch(`https://api.polygon.io/v3/reference/tickers/${params.slug}?apiKey=rLdG0SNWBFY4CfpFmtA1l3uMpo7k5eXR`).then((res) => res.json());
 
 	return data;
 }
 
-async function getChartData({ params }: { params: { slug: string } }) {
-	const data = await fetch(`https://api.polygon.io/v2/aggs/ticker/${params.slug}/range/1/day/2024-07-10/2024-07-17?adjusted=true&sort=asc&apiKey=rLdG0SNWBFY4CfpFmtA1l3uMpo7k5eXR`).then((res) => res.json());
-
-	return data;
-}
-
 export default async function Page({ params }: { params: { slug: string } }) {
-	const detailData = await getDetailData({ params });
-	const chartData = await getChartData({ params });
-
-	const labels = chartData.results?.map((result: { t: number }) => new Date(result.t).toLocaleDateString());
-	const values = chartData.results?.map((result: { c: number }) => result.c);
-
-	const chartParams = {
-		labels: labels,
-		values: values,
-	};
+	const data = await getData({ params });
 
 	return (
 		<div className="max-w-xl mx-auto p-8 space-y-8 border border-neutral-300 rounded-lg">
 			<div className="space-y-4">
-				<h1 className="text-3xl font-black">{detailData.results.name}</h1>
-				{detailData.results.description && <p>{detailData.results.description}</p>}
+				<h1 className="text-3xl font-black">{data.results.name}</h1>
+				{data.results.description && <p>{data.results.description}</p>}
 			</div>
-			{chartData.results && <PriceChart params={chartParams} />}
+			<PriceChart slug={params.slug} />
 		</div>
 	);
 }
